82ebe7c94dbc49f2e423f8d90329e2f827506cb1
Jhelen <jhelenquintas2006@gmail.com>
2025-05-29 21:59:19 -0500

    Integration bootstrap template

---
+++ src/config/database.php
@@ -0,0 +1,31 @@
+<?php
+
+    $host = "aws-0-us-east-1.pooler.supabase.com";
+    $port = "6543";
+    $dbname = "postgres";
+    $user = "postgres.dessvximbvmdkuxrggmp";
+    $password = "unicesmag@@";
+/*
+    $host = "localhost";
+    $port = "5432";
+    $dbname = "petstore";
+    $user = "postgres";
+    $password = "unicesmag";
+*/
+    $data_connection = "
+    host=$host
+    port=$port
+    dbname=$dbname
+    user=$user
+    password=$password
+    ";
+    
+    $conn = pg_connect($data_connection);
+
+    if(!$conn){
+        echo "Connection error";
+    } else {
+        echo "sucess!!";
+    }
+
+?>
